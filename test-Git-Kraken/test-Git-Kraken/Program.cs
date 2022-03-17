16dbd550e76ec4074b474d5f66ed71633571f2e2
Vinicius Mendes <costavm@taginfraestrutura.com.br>
2022-03-17 16:30:41 -0300

    Criado Projeto Inicial

---
+++ test-Git-Kraken/test-Git-Kraken/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test_Git_Kraken
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
